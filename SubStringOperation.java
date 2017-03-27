138fcb7dfcbc436b5bd3c89aec199571c39a2786
arjun541 <bmkr.541@gmail.com>
2017-03-27 22:33:50 +0530

    Create SubStringOperation.java

---
+++ SubStringOperation.java
@@ -0,0 +1,22 @@
+
+package com;
+
+
+
+public class SubStringOperation {
+
+		public static void main(String args[])
+		{
+StringBuilder sb=new StringBuilder("welcome");
+String s="come";
+if(sb.indexOf(s)==-1)
+{
+	System.out.println(s+" is found as Substring in "+sb);
+}
+else
+	System.out.println(s+" is  found as Substring in "+sb);
+		}
+			
+
+		}
+
